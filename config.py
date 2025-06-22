1612ef13e7bbf382762f71a8cc3709bee54fb5fc
Chayan-03 <chayan.06014802722@cse.mait.ac.in>
2025-06-22 11:44:22 +0530

    Clean history, removed sensitive files

--- config.py
+++ config.py
@@ -1,8 +1,3 @@
-# DISCORD_TOKEN ="MTM4NjAyNTMxMTcyMzk4MzAwOA.G_3v5L.bvKWG4jV4ORw9ZWNk9StuzSk7DHN2LD8I-BlA8"
-# CHANNEL_ID = 1386024145342697475  # Replace with your actual #contest-updates channel ID as an integer
-# CLIST_API_KEY ="Explorer_the_gr8:bda0f0a94f3edef50553e20a2841033001b277f6"
-
-
 from dotenv import load_dotenv
 import os
 
