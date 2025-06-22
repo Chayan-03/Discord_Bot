670beb25c85bab2f733521d39e52798c9f506137
Chayan-03 <chayan.06014802722@cse.mait.ac.in>
2025-06-22 11:32:10 +0530

    Removed secrets and added .env support

--- config.py
+++ config.py
@@ -1,3 +1,14 @@
-DISCORD_TOKEN ="MTM4NjAyNTMxMTcyMzk4MzAwOA.G_3v5L.bvKWG4jV4ORw9ZWNk9StuzSk7DHN2LD8I-BlA8"
-CHANNEL_ID = 1386024145342697475  # Replace with your actual #contest-updates channel ID as an integer
-CLIST_API_KEY ="Explorer_the_gr8:bda0f0a94f3edef50553e20a2841033001b277f6"
+# DISCORD_TOKEN ="MTM4NjAyNTMxMTcyMzk4MzAwOA.G_3v5L.bvKWG4jV4ORw9ZWNk9StuzSk7DHN2LD8I-BlA8"
+# CHANNEL_ID = 1386024145342697475  # Replace with your actual #contest-updates channel ID as an integer
+# CLIST_API_KEY ="Explorer_the_gr8:bda0f0a94f3edef50553e20a2841033001b277f6"
+
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+CLIST_API_KEY = os.getenv("CLIST_API_KEY")
+CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
+
